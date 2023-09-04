fa6a832db178d6293b42f949d11c7e7598e7cb5a
Ben Bilgili <benbilgili@gmail.com>
2023-09-04 09:42:24 +0100

    instructions added

--- php_codewars.php
+++ php_codewars.php
@@ -1,10 +1,19 @@
+<!-- Complete the solution so that it splits the string into pairs of two characters. 
+If the string contains an odd number of characters then it should replace the missing second character 
+of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef'] -->
+
 <?php 
 
 function solution($str) {
   if (empty($str)) {
     return [];
   }
-  
+
   $array = str_split($str, 1);
   $split = array_chunk($array, 2);
   $result = [];
